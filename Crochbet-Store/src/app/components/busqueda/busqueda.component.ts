492f42975977ffa0c848252f5f9acd8a01facb16
Milton Elizalde <alexmol_14@outlook.com>
2023-01-11 17:16:01 -0500

    nuevos cambios

--- Crochbet-Store/src/app/components/busqueda/busqueda.component.ts
+++ Crochbet-Store/src/app/components/busqueda/busqueda.component.ts
@@ -3,7 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { window } from 'rxjs';
 import { Product } from 'src/app/interfaces/product';
+import { CarritoModule } from 'src/app/modules/carrito/carrito.module';
 import { ProductoModule } from 'src/app/modules/producto/producto.module';
+import Swal from 'sweetalert2';
 import { CarritoComponent } from '../carrito/carrito.component';
 
 @Component({
@@ -67,4 +69,37 @@ export class BusquedaComponent {
     this.dialog.open(CarritoComponent,{});
   }
 
+  
+  carrito=[{}];
+  confirmation:any;
+  verProducto(indice:number){
+    indice=indice-1;
+    this.confirmation=Swal.fire({
+      title: this.moduloProducto[indice].title,
+      text: "Quieres agregar este producto al carrito?",
+      html: "<p>"+this.moduloProducto[indice].description+"</p>"
+      +"<p>Precio: $"+this.moduloProducto[indice].price+"</p>",
+      imageUrl: this.moduloProducto[indice].image,
+      imageWidth: 250,
+      imageHeight: 225,
+      imageAlt: this.moduloProducto[indice].image,
+      showCancelButton: true,
+      confirmButtonText: "AGREGAR",
+      cancelButtonText: "SALIR",
+      confirmButtonColor: "black",
+      cancelButtonColor: "red",
+      reverseButtons: true
+    }).then ((result) => {
+      if(result.isConfirmed){
+        CarritoModule.addOnCarrito(this.moduloProducto[indice]);
+        Swal.fire(
+          'Producto agregado',
+          'El producto ha sido agregado al carrito',
+          'success'
+        )
+        }
+        this.carrito.push(this.moduloProducto[indice]);
+        console.log(this.carrito);
+      })
+  }
 }
