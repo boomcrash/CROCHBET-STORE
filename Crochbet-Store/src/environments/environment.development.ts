f5d8829c005efbba641bd2172149358c1fcb3296
Milton Elizalde <alexmol_14@outlook.com>
2023-03-14 09:06:43 -0500

    cambios nuevos

--- Crochbet-Store/src/environments/environment.development.ts
+++ Crochbet-Store/src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
     roles:['1', '2', '3'],
+    url_base:"https://localhost:7235/api/",
+    //-------------- Productos ----------------
+    url_getProductos:"Producto/getProductos",
+    url_getProductosCategories:"Producto/getProductosCategories",
 };
